bfb74d6300e204e26538c6e483ee286bb2c3fd67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 16:05:53 +0300

    Throwing an HTTPException instead of returning an object with an error entry

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -2,11 +2,13 @@ import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono().get('/', clerkMiddleware(), async (c) => {
 	const auth = getAuth(c)
 
-	if (!auth?.userId) return c.json({ error: 'Unauthorized' }, 401)
+	if (!auth?.userId)
+		throw new HTTPException(401, { res: c.json({ error: 'Unauthorized' }) })
 
 	const data = await db
 		.select({ id: accounts.id, name: accounts.name })
