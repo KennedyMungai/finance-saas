a3e3a666b79f0687ef1be60aca11350767e5a0c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 12:33:06 +0300

    Created a not-found page for the application

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+const NotFound = () => {
+	return (
+		<div className='h-screen bg-blue-400 flex flex-col items-center justify-center gap-4 font-bold text-3xl text-white'>
+			404 - Page Not Found
+			<Button variant={'outline'} asChild className='bg-transparent'>
+				<Link href={'/'}>Go Back</Link>
+			</Button>
+		</div>
+	)
+}
+
+export default NotFound
