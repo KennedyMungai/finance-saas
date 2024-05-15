22d7142b0d5d512310bf1ee2dfaa467c5b7160c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 14:52:53 +0300

    Added the plaidId field

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,6 +2,7 @@ import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
 export const accounts = pgTable('accounts', {
 	id: uuid('id').defaultRandom().primaryKey(),
+	plaidId: uuid('plaid_id').defaultRandom().notNull(),
 	name: text('name').notNull(),
 	userId: text('user_id').notNull()
 })
