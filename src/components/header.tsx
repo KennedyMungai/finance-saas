029b7a673c47f133c14fa054fc3cad335e241d03
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 12:44:04 +0300

    Added a user button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from '@clerk/nextjs'
 import HeaderLogo from './header-logo'
 import Navigation from './navigation'
 
@@ -10,6 +11,7 @@ const Header = () => {
 						<HeaderLogo />
 						<Navigation />
 					</div>
+					<UserButton afterSignOutUrl='/' />
 				</div>
 			</div>
 		</header>
