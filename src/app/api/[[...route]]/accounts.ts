c7c9322fd7459383d00e3f7e4a2d940734bda25b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 15:55:07 +0300

    Implemented some authentication for the api route

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,8 +1,13 @@
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { Hono } from 'hono'
 
-const app = new Hono().get('/', async (c) => {
+const app = new Hono().get('/', clerkMiddleware(), async (c) => {
+	const auth = getAuth(c)
+
+	if (!auth?.userId) c.json({ error: 'Unauthorized' }, 401)
+
 	const data = await db
 		.select({ id: accounts.id, name: accounts.name })
 		.from(accounts)
