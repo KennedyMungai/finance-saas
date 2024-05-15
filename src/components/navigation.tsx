a97b3ff643991b605383014fd4baab52a8360710
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 12:25:13 +0300

    Added some additional styles for the navigation buttons

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import NavButton from './nav-button'
+import NavButton from '@/components/nav-button'
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Menu } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useMedia } from 'react-use'
 
 const routes = [
 	{
@@ -27,7 +32,50 @@ const routes = [
 ]
 
 const Navigation = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const router = useRouter()
 	const pathname = usePathname()
+
+	const isMobile = useMedia('(max-width: 1024px)', false)
+
+	const onClick = (href: string) => {
+		router.push(href)
+		setIsOpen(false)
+	}
+
+	if (isMobile) {
+		return (
+			<Sheet open={isOpen} onOpenChange={setIsOpen}>
+				<SheetTrigger>
+					<Button
+						variant={'outline'}
+						size='sm'
+						className='font-normal bg-white/10 hover:bg-white/20 hover:text-white focus-visible:ring-transparent focus-visible:ring-offset-0 outline-none text-white focus:bg-white/30 transition border-none'
+					>
+						<Menu className='size-4' />
+					</Button>
+				</SheetTrigger>
+				<SheetContent side={'left'} className='px-2'>
+					<nav className='flex flex-col gap-y-2 pt-6'>
+						{routes.map((route) => (
+							<Button
+								key={route.href}
+								variant={
+									route.href === pathname
+										? 'secondary'
+										: 'ghost'
+								}
+								onClick={() => onClick(route.href)}
+							>
+								{route.label}
+							</Button>
+						))}
+					</nav>
+				</SheetContent>
+			</Sheet>
+		)
+	}
 
 	return (
 		<nav className='hidden lg:flex items-center gap-x-2 overflow-x-auto'>
@@ -36,7 +84,7 @@ const Navigation = () => {
 					key={index}
 					href={route.href}
 					label={route.label}
-					isActive={pathname === route.href}
+					isActive={pathname == route.href}
 				/>
 			))}
 		</nav>
