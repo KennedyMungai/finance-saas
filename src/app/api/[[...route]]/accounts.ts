047a765857c8cb130c173aab1e8f268ddae7e48b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 16:28:58 +0300

    Added a filter for the accounts userId

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,6 +1,7 @@
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 import { HTTPException } from 'hono/http-exception'
 
@@ -15,6 +16,7 @@ const app = new Hono().get('/', clerkMiddleware(), async (c) => {
 	const data = await db
 		.select({ id: accounts.id, name: accounts.name })
 		.from(accounts)
+		.where(eq(accounts.userId, auth?.userId))
 	return c.json({ data })
 })
 
