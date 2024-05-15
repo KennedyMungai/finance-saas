dddbe4986b0fa142035f01b487c765d1b4a682bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-15 15:09:03 +0300

    Some api routing information

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,13 +2,16 @@ import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import accounts from './accounts'
 import { z } from 'zod'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => c.json({ message: 'Hello world' }))
+const routes = app.route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
