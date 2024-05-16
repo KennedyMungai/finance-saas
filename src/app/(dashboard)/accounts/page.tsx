b475c401641e47525d8a82aeb52a335f22e5e0ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-16 11:39:08 +0300

    Set up the add new button to open a sheet that has the form that creates a new account in the database

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
+import { Plus } from 'lucide-react'
+
 const AccountsPage = () => {
-	return <div>AccountsPage</div>
+	const newAccount = useNewAccount()
+
+	return (
+		<div>
+			<Card className='border-none drop-shadow-sm'>
+				<CardHeader className='flex gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
+					<CardTitle className='text-xl line-clamp-1'>
+						AccountsPage
+					</CardTitle>
+					<Button size={'sm'} onClick={newAccount.onOpen}>
+						<Plus className='size-4 mr-2' />
+						Add new
+					</Button>
+				</CardHeader>
+			</Card>
+		</div>
+	)
 }
 
 export default AccountsPage
