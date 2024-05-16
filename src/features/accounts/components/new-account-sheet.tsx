de95395ea86300cec1b5568131193df5cca87a5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-16 11:11:05 +0300

    Added the logic to add new account info to the database

--- src/features/accounts/components/new-account-sheet.tsx
+++ src/features/accounts/components/new-account-sheet.tsx
@@ -5,11 +5,22 @@ import {
 	SheetHeader,
 	SheetTitle
 } from '@/components/ui/sheet'
+import { insertAccountsSchema } from '@/db/schema'
+import { useCreateAccount } from '@/features/accounts/api/use-create-account'
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
+import { z } from 'zod'
 import AccountForm from './account-form'
+
+const formSchema = insertAccountsSchema.pick({ name: true })
+
+type FormValues = z.infer<typeof formSchema>
 
 const NewAccountSheet = () => {
 	const { isOpen, onClose } = useNewAccount()
+
+	const mutation = useCreateAccount()
+
+	const onSubmit = (values: FormValues) => mutation.mutate(values)
 
 	return (
 		<Sheet open={isOpen} onOpenChange={onClose}>
@@ -20,7 +31,11 @@ const NewAccountSheet = () => {
 						Create a new account to track your transactions
 					</SheetDescription>
 				</SheetHeader>
-				<AccountForm onSubmit={() => {}} disabled={false} />
+				<AccountForm
+					onSubmit={onSubmit}
+					disabled={false}
+					defaultValues={{ name: '' }}
+				/>
 			</SheetContent>
 		</Sheet>
 	)
