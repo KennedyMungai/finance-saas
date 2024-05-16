cae8100b49d3af912294dcde0b84a190c921b68c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-16 08:52:10 +0300

    Added an insertAccountSchema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { createInsertSchema } from 'drizzle-zod'
 
 export const accounts = pgTable('accounts', {
 	id: uuid('id').defaultRandom().primaryKey(),
@@ -6,3 +7,5 @@ export const accounts = pgTable('accounts', {
 	name: text('name').notNull(),
 	userId: text('user_id').notNull()
 })
+
+export const insertAccountsSchema = createInsertSchema(accounts)
