b22d9ab285993b423c657880ec3bc968c63e0b82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-17 12:34:59 +0300

    Updated the color of the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const NotFound = () => {
 	return (
-		<div className='h-screen bg-blue-600 flex flex-col items-center justify-center gap-4 font-bold text-3xl text-white'>
+		<div className='h-screen bg-blue-500 flex flex-col items-center justify-center gap-4 font-bold text-3xl text-white'>
 			404 - Page Not Found
 			<Button variant={'outline'} asChild className='bg-transparent'>
 				<Link href={'/'}>Go Back</Link>
