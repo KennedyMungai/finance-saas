b6e0ba94d8611c4ae7a5c6099ff3d00232bd80be
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-17 10:57:36 +0300

    Implemented the delete confirmation logic

--- src/features/accounts/components/edit-account-sheet.tsx
+++ src/features/accounts/components/edit-account-sheet.tsx
@@ -13,6 +13,7 @@ import { useOpenAccount } from '@/features/accounts/hooks/use-open-account'
 import { Loader2 } from 'lucide-react'
 import { z } from 'zod'
 import AccountForm from './account-form'
+import { useConfirm } from '@/hooks/use-confirm'
 
 const formSchema = insertAccountsSchema.pick({ name: true })
 
@@ -20,6 +21,11 @@ type FormValues = z.infer<typeof formSchema>
 
 const EditAccountSheet = () => {
 	const { isOpen, onClose, id } = useOpenAccount()
+
+	const [ConfirmDelete, confirm] = useConfirm(
+		'Are you sure?',
+		'You are about to delete this transaction'
+	)
 
 	const accountsQuery = useGetAccount(id)
 	const editMutation = useEditAccount(id)
@@ -36,34 +42,43 @@ const EditAccountSheet = () => {
 		? { name: accountsQuery.data.name }
 		: { name: '' }
 
+	const onDelete = async () => {
+		const ok = await confirm()
+
+		if (ok) deleteMutation.mutate(undefined, { onSuccess: () => onClose() })
+	}
+
 	return (
-		<Sheet open={isOpen} onOpenChange={onClose}>
-			<SheetContent className='space-y-4'>
-				<SheetHeader>
-					<SheetTitle>
-						{id ? 'Edit Account' : 'New Account'}
-					</SheetTitle>
-					<SheetDescription>
-						{id
-							? 'Edit an existing account'
-							: 'Create a new account to track your transactions'}
-					</SheetDescription>
-				</SheetHeader>
-				{isLoading ? (
-					<div className='absolute inset-0 flex items-center justify-center'>
-						<Loader2 className='text-muted-foreground animate-spin size-8' />
-					</div>
-				) : (
-					<AccountForm
-						id={id}
-						onSubmit={onSubmit}
-						disabled={isPending}
-						defaultValues={defaultValues}
-						onDelete={deleteMutation.mutate}
-					/>
-				)}
-			</SheetContent>
-		</Sheet>
+		<>
+			<ConfirmDelete />
+			<Sheet open={isOpen} onOpenChange={onClose}>
+				<SheetContent className='space-y-4'>
+					<SheetHeader>
+						<SheetTitle>
+							{id ? 'Edit Account' : 'New Account'}
+						</SheetTitle>
+						<SheetDescription>
+							{id
+								? 'Edit an existing account'
+								: 'Create a new account to track your transactions'}
+						</SheetDescription>
+					</SheetHeader>
+					{isLoading ? (
+						<div className='absolute inset-0 flex items-center justify-center'>
+							<Loader2 className='text-muted-foreground animate-spin size-8' />
+						</div>
+					) : (
+						<AccountForm
+							id={id}
+							onSubmit={onSubmit}
+							disabled={isPending}
+							defaultValues={defaultValues}
+							onDelete={onDelete}
+						/>
+					)}
+				</SheetContent>
+			</Sheet>
+		</>
 	)
 }
 
