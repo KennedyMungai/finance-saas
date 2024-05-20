52b95c19bcba197117b4ea66be4c7915c07031cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-20 10:34:32 +0300

    Some tranaction logic and markup for the application

---
+++ src/features/transactions/components/edit-transaction-sheet.tsx
@@ -0,0 +1,85 @@
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle
+} from '@/components/ui/sheet'
+import { insertAccountsSchema } from '@/db/schema'
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account'
+import { useEditAccount } from '@/features/accounts/api/use-edit-account'
+import { useGetAccount } from '@/features/accounts/api/use-get-account'
+import { useOpenAccount } from '@/features/accounts/hooks/use-open-account'
+import { Loader2 } from 'lucide-react'
+import { z } from 'zod'
+import TransactionForm from './transaction-form'
+import { useConfirm } from '@/hooks/use-confirm'
+
+const formSchema = insertAccountsSchema.pick({ name: true })
+
+type FormValues = z.infer<typeof formSchema>
+
+const EditTransactionSheet = () => {
+	const { isOpen, onClose, id } = useOpenAccount()
+
+	const [ConfirmDelete, confirm] = useConfirm(
+		'Are you sure?',
+		'You are about to delete this account'
+	)
+
+	const accountsQuery = useGetAccount(id)
+	const editMutation = useEditAccount(id)
+	const deleteMutation = useDeleteAccount(id)
+
+	const isLoading = accountsQuery.isLoading
+
+	const isPending = editMutation.isPending || deleteMutation.isPending
+
+	const onSubmit = (values: FormValues) =>
+		editMutation.mutate(values, { onSuccess: () => onClose() })
+
+	const defaultValues = accountsQuery.data
+		? { name: accountsQuery.data.name }
+		: { name: '' }
+
+	const onDelete = async () => {
+		const ok = await confirm()
+
+		if (ok) deleteMutation.mutate(undefined, { onSuccess: () => onClose() })
+	}
+
+	return (
+		<>
+			<ConfirmDelete />
+			<Sheet open={isOpen} onOpenChange={onClose}>
+				<SheetContent className='space-y-4'>
+					<SheetHeader>
+						<SheetTitle>
+							{id ? 'Edit Account' : 'New Account'}
+						</SheetTitle>
+						<SheetDescription>
+							{id
+								? 'Edit an existing account'
+								: 'Create a new account to track your transactions'}
+						</SheetDescription>
+					</SheetHeader>
+					{isLoading ? (
+						<div className='absolute inset-0 flex items-center justify-center'>
+							<Loader2 className='text-muted-foreground animate-spin size-8' />
+						</div>
+					) : (
+						<TransactionForm
+							id={id}
+							onSubmit={onSubmit}
+							disabled={isPending}
+							defaultValues={defaultValues}
+							onDelete={onDelete}
+						/>
+					)}
+				</SheetContent>
+			</Sheet>
+		</>
+	)
+}
+
+export default EditTransactionSheet
---
+++ src/features/transactions/components/transaction-form.tsx
@@ -0,0 +1,196 @@
+import AmountInput from '@/components/amount-input'
+import DatePicker from '@/components/date-picker'
+import Select from '@/components/select'
+import { Button } from '@/components/ui/button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { insertTransactionsSchema } from '@/db/schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Trash } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+	date: z.coerce.date(),
+	accountId: z.string(),
+	categoryId: z.string().nullable().optional(),
+	payee: z.string(),
+	amount: z.string(),
+	notes: z.string().nullable().optional()
+})
+
+const apiSchema = insertTransactionsSchema.omit({ id: true })
+
+type FormValues = z.input<typeof formSchema>
+type ApiFormValues = z.input<typeof apiSchema>
+
+type Props = {
+	id?: string
+	defaultValues?: FormValues
+	onSubmit: (values: ApiFormValues) => void
+	onDelete?: () => void
+	disabled?: boolean
+	accountOptions: { label: string; value: string }[]
+	categoryOptions: { label: string; value: string }[]
+	onCreateAccount: (name: string) => void
+	onCreateCategory: (name: string) => void
+}
+
+const TransactionForm = ({
+	onSubmit,
+	defaultValues,
+	disabled,
+	id,
+	onDelete,
+	accountOptions,
+	categoryOptions,
+	onCreateAccount,
+	onCreateCategory
+}: Props) => {
+	const form = useForm<FormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: defaultValues
+	})
+
+	const handleSubmit = (values: FormValues) => {
+		// onSubmit(values)
+		console.log({ values })
+	}
+
+	const handleDelete = () => onDelete?.()
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(handleSubmit)}
+				className='space-y-4 pt-4'
+			>
+				<FormField
+					name='date'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormControl>
+								<DatePicker
+									value={field.value}
+									onChange={field.onChange}
+									disabled={disabled}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name='accountId'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Account</FormLabel>
+							<FormControl>
+								<Select
+									placeholder='Select an Account'
+									options={accountOptions}
+									onCreate={onCreateAccount}
+									value={field.value}
+									onChange={field.onChange}
+									disabled={disabled}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name='categoryId'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Category</FormLabel>
+							<FormControl>
+								<Select
+									placeholder='Select a category'
+									options={categoryOptions}
+									onCreate={onCreateCategory}
+									value={field.value}
+									onChange={field.onChange}
+									disabled={disabled}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name='payee'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Payee</FormLabel>
+							<FormControl>
+								<Input
+									disabled={disabled}
+									placeholder='Add a payee'
+									{...field}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name='amount'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Amount</FormLabel>
+							<FormControl>
+								<AmountInput
+									{...field}
+									disabled={disabled}
+									placeholder='0.00'
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name='notes'
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Notes</FormLabel>
+							<FormControl>
+								<Textarea
+									{...field}
+									value={field.value ?? ''}
+									disabled={disabled}
+									placeholder='Optional notes'
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<Button className='w-full' disabled={disabled}>
+					{id ? 'Save Changes' : 'Create Account'}
+				</Button>
+				{!!id && (
+					<Button
+						className='w-full'
+						type='button'
+						disabled={disabled}
+						onClick={handleDelete}
+						variant={'outline'}
+					>
+						<Trash className='size-4 mr-3' /> Delete Account
+					</Button>
+				)}
+			</form>
+		</Form>
+	)
+}
+
+export default TransactionForm
