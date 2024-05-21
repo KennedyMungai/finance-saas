3330978ee2534858b915651f52de17e794844718
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-21 14:25:15 +0300

    Created and rendered the DataGrid component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-	return <div>Home Page</div>
+'use client'
+
+import DataGrid from '@/components/data-grid'
+
+const DashboardPage = () => {
+	return (
+		<div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+			<DataGrid />
+		</div>
+	)
 }
+
+export default DashboardPage
---
+++ src/components/data-grid.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const DataGrid = (props: Props) => {
+	return <div>DataGrid</div>
+}
+
+export default DataGrid
