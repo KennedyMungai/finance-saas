4d331b67cdef288c314b0569db7423e76826468a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-21 10:37:45 +0300

    Updated the import order for the file

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
 import categories from './categories'
-import transactions from './transactions'
 import summary from './summary'
+import transactions from './transactions'
 
 export const runtime = 'edge'
 
