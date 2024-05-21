f073fb7c8073df5b697bd84f2b78e88002d59024
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-21 15:23:56 +0300

    Defined the formatPercentage function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -73,3 +73,16 @@ export const formatDateRange = (period?: Period) => {
 
 	return format(period.from, 'LLL dd, y')
 }
+
+export const formatPercentage = (
+	value: number,
+	options: { addPrefix?: boolean } = { addPrefix: false }
+) => {
+	const result = new Intl.NumberFormat('en-US', { style: 'percent' }).format(
+		value / 100
+	)
+
+	if (options.addPrefix && value > 0) return `+${result}`
+
+	return result
+}
