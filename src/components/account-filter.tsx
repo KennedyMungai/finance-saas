b17f5c3fb66759e4500346b4d197e74f8ea783b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-22 11:48:20 +0300

    Removed all the unused imports

--- src/components/account-filter.tsx
+++ src/components/account-filter.tsx
@@ -1,21 +1,13 @@
 'use client'
 
-import qs from 'query-string'
 import {
 	Select,
 	SelectContent,
-	SelectGroup,
 	SelectItem,
-	SelectLabel,
-	SelectScrollDownButton,
-	SelectScrollUpButton,
-	SelectSeparator,
 	SelectTrigger,
 	SelectValue
 } from '@/components/ui/select'
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
 
 type Props = {}
 
