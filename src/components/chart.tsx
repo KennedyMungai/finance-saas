09110878496210496e831746e81f4f5fe0e20924
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-22 10:14:55 +0300

    Made some updates for the select component

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,8 +1,21 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { FileSearch } from 'lucide-react'
+import { AreaChart, BarChart, FileSearch, LineChart } from 'lucide-react'
 import AreaVariant from './area-variant'
 import BarVariant from './bar-variant'
 import LineVariant from './line-variant'
+import { useState } from 'react'
+import {
+	Select,
+	SelectContent,
+	SelectTrigger,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectScrollDownButton,
+	SelectScrollUpButton,
+	SelectSeparator,
+	SelectValue
+} from '@/components/ui/select'
 
 type Props = {
 	data?: {
@@ -13,13 +26,44 @@ type Props = {
 }
 
 const Chart = ({ data = [] }: Props) => {
+	const [chartType, setChartType] = useState('area')
+
+	const onTypeChange = (type: 'area' | 'bar' | 'line') => {
+		// TODO: Add paywall
+		setChartType(type)
+	}
+
 	return (
 		<Card className='border-none drop-shadow-sm'>
 			<CardHeader className='flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between'>
 				<CardTitle className='text-xl line-clamp-1'>
 					Transactions
 				</CardTitle>
-				{/* TODO: Add select */}
+				<Select defaultValue={chartType} onValueChange={onTypeChange}>
+					<SelectTrigger className='lg:w-auto h-9 rounded-md px-3 focus:ring-0 active:ring-0'>
+						<SelectValue placeholder='Chart Type' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value='area'>
+							<div className='flex items-center'>
+								<AreaChart className='size-4 mr-2 shrink-0' />
+								<p className='line-clamp-1'>Area Chart</p>
+							</div>
+						</SelectItem>
+						<SelectItem value='line'>
+							<div className='flex items-center'>
+								<LineChart className='size-4 mr-2 shrink-0' />
+								<p className='line-clamp-1'>Line Chart</p>
+							</div>
+						</SelectItem>
+						<SelectItem value='bar'>
+							<div className='flex items-center'>
+								<BarChart className='size-4 mr-2 shrink-0' />
+								<p className='line-clamp-1'>Bar Chart</p>
+							</div>
+						</SelectItem>
+					</SelectContent>
+				</Select>
 			</CardHeader>
 			<CardContent>
 				{data.length === 0 ? (
